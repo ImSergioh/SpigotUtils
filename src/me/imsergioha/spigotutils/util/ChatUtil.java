4c31c66bb41cde233a2d453a38a589270cb0c5ac
ImSergioh <50914599+ImSergioh@users.noreply.github.com>
2022-11-19 10:46:03 +0100

    update

---
+++ src/me/imsergioha/spigotutils/util/ChatUtil.java
@@ -0,0 +1,9 @@
+package me.imsergioha.spigotutils.util;
+
+public class ChatUtil {
+
+    public static String chatColor(String message){
+        return message.replace("&", "§");
+    }
+
+}
---
+++ src/me/imsergioha/spigotutils/util/LocationsUtil.java
@@ -0,0 +1,18 @@
+package me.imsergioha.spigotutils.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class LocationsUtil {
+
+    public static String locationToString(Location location){
+        return location.getWorld().getName()+";"+location.getX()+";"+location.getY()+";"+location.getZ()+";"+location.getYaw()+";"+location.getPitch();
+    }
+
+    public static Location stringToLocation(String locationString){
+        String[] args = locationString.split(";");
+        return new Location(Bukkit.getWorld(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3]),
+        Float.parseFloat(args[4]), Float.parseFloat(args[5]));
+    }
+
+}
